7d2d95e63d4abc47ebb44b2a759148e2388e8bec
Jordi Martinez Joyeux <jmartinezjoy@uoc.edu>
2023-01-12 11:42:51 +0100

    update api node

--- src/UserService.js
+++ src/UserService.js
@@ -32,7 +32,7 @@ export class User {
          chainId: 'f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12',
          rpcEndpoints: [{
              protocol: 'https',
-             host: 'testnet-wax.3dkrender.com',
+             host: 'tapiwax.3dkrender.com',
              port: ''
          }]
      };
